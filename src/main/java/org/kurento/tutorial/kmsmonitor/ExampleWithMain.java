14714aa4c8cef3338ac89d9cd12642cf1c8a604a
root <root@kurentokms.kurentokms.g5.internal.cloudapp.net>
2015-05-16 18:50:00 +0000

    push to graphite

--- src/main/java/org/kurento/tutorial/kmsmonitor/ExampleWithMain.java
+++ src/main/java/org/kurento/tutorial/kmsmonitor/ExampleWithMain.java
@@ -1,9 +1,12 @@
 package org.kurento.tutorial.kmsmonitor;
+import java.io.DataOutputStream;
+import java.net.Socket;
 
 public class ExampleWithMain {
 	
 	final static String DEFAULT_KMS_WS_URI = "ws://localhost:8888/kurento";
-	
+	final static String DEFAULT_GRAPHITE_IP = "80.96.122.69";	
+
 	public static void main(String[] args) throws InterruptedException {
 		KmsMonitor kmsMonitor = new KmsMonitor(System.getProperty("kms.ws.uri", DEFAULT_KMS_WS_URI));
 		
@@ -17,6 +20,10 @@ public class ExampleWithMain {
 			System.out.println("Inbound.Delta.Nacks, "+ kmsStats.getWebRtcStats().getInbound().getDeltaNacks());
 			System.out.println("Inbound.Jitter, " + kmsStats.getWebRtcStats().getInbound().getJitter());
 			
+			Socket conn = new Socket(System.getProperty("graphite_ip", DEFAULT_GRAPHITE_IP), 2003);
+			DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+			dos.writeBytes("server.app1tt.pipelines "+ kmsStats.getNumPipelines() +"\n");
+			conn.close();
 			
 			Thread.sleep(3000);
 		}	
