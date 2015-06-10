14f76c5a7a0cb54d082981ff7dacb320f02b58a3
Cristian Spoiala <scristian@gmail.com>
2015-06-10 15:50:47 +0000

    cleanup

--- src/main/java/org/kurento/tutorial/kmsmonitor/SendStats.java
+++ src/main/java/org/kurento/tutorial/kmsmonitor/SendStats.java
@@ -21,22 +21,15 @@ public class SendStats {
 		
 		while(true){
 			KmsStats kmsStats = kmsMonitor.updateStats();
-			//System.out.println("Num.Pipelines, " + kmsStats.getNumPipelines());
-			//System.out.println("Num.Elements, " + kmsStats.getNumElements());
-			//System.out.println("Num.WebRtcEndpoints, " + kmsStats.getNumWebRtcEndpoints());
-			
-			//System.out.println("Inbound.Byte.Count, "+ kmsStats.getWebRtcStats().getInbound().getByteCount());
-			//System.out.println("Inbound.Delta.Nacks, "+ kmsStats.getWebRtcStats().getInbound().getDeltaNacks());
-			//System.out.println("Inbound.Jitter, " + kmsStats.getWebRtcStats().getInbound().getJitter());
 			
 			try {
 				Socket conn = new Socket(graphite_ip, 2003);
 				DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+				
 				System.out.println("Sending stats for hostname: " + hostname + " to graphite... ");
 				dos.writeBytes("server."+hostname+".pipelines " + kmsStats.getNumPipelines() +" "+ System.currentTimeMillis() / 1000L +"\n");
-				//dos.writeBytes("server."+hostname+".pipelines 12 "+ System.currentTimeMillis() / 1000L +"\n");
 				dos.writeBytes("server."+hostname+".elements " + kmsStats.getNumElements() +" "+ System.currentTimeMillis() / 1000L +"\n");
-				//dos.writeBytes("server."+hostname+".elements 15 "+ System.currentTimeMillis() / 1000L +"\n");
+				
 				conn.close();
 			} catch (Exception e) {
 				System.out.println(e);
