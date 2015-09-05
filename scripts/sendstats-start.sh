1c8496fb2325079a2359aaa5b6edf922d039378d
Cristian Spoiala <scristian@Cristians-MacBook-Pro.local>
2015-09-06 00:23:48 +0300

    better init script

--- scripts/sendstats-start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /home/ubuntu/kms-monitoring-java
-nohup java -cp httpcore-4.3.1.jar:websocket-api-9.3.0.M1.jar:jetty-io-9.3.0.M1.jar:jetty-util-9.3.0.M1.jar:websocket-common-9.3.0.M1.jar:websocket-client-9.3.0.M1.jar:gson-2.3.1.jar:guava-18.0.jar:slf4j-api-1.7.5.jar:kurento-jsonrpc-client-6.0.0-SNAPSHOT.jar:kurento-commons-6.0.0-SNAPSHOT.jar:kurento-client-6.0.0-SNAPSHOT.jar:target/kms-monitoring-java-6.0.0-SNAPSHOT.jar org.kurento.tutorial.kmsmonitor.SendStats &
--- scripts/sendstats-stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Grabs and kill a process from the pidlist
-
-pid=`ps aux | grep SendStats | awk '{print $2}'`
-kill -9 $pid
