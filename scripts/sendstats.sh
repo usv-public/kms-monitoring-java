3c0b88a193aeb50698b9b42cfccb4ffd1c9d3634
Alin CALINCIUC <alin.calinciuc@gmail.com>
2016-04-13 20:42:06 +0300

    Update sendstats.sh

--- scripts/sendstats.sh
+++ scripts/sendstats.sh
@@ -1,6 +1,8 @@
+#!/bin/bash -ex
 echo -n "Starting application: "
 sleep 30
+touch /var/log/sendstats.log
 # Redirects default and error output to a log file
 EXTERNAL_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 applDir="/root/kms-monitoring-java"
-java -Dkms.ws.uri=ws://$EXTERNAL_IP:8888/kurento -cp $applDir/httpcore-4.3.1.jar:websocket-api-9.3.0.M1.jar:jetty-io-9.3.0.M1.jar:jetty-util-9.3.0.M1.jar:websocket-common-9.3.0.M1.jar:websocket-client-9.3.0.M1.jar:gson-2.3.1.jar:guava-18.0.jar:slf4j-api-1.7.5.jar:kurento-jsonrpc-client-6.0.0-SNAPSHOT.jar:kurento-commons-6.0.0-SNAPSHOT.jar:kurento-client-6.0.0-SNAPSHOT.jar:$applDir/target/kms-monitoring-java-6.0.0-SNAPSHOT.jar org.kurento.tutorial.kmsmonitor.SendStats
+cd $applDir && java -Dkms.ws.uri=ws://$EXTERNAL_IP:8888/kurento -cp httpcore-4.3.1.jar:websocket-api-9.3.0.M1.jar:jetty-io-9.3.0.M1.jar:jetty-util-9.3.0.M1.jar:websocket-common-9.3.0.M1.jar:websocket-client-9.3.0.M1.jar:gson-2.3.1.jar:guava-18.0.jar:slf4j-api-1.7.5.jar:kurento-jsonrpc-client-6.0.0-SNAPSHOT.jar:kurento-commons-6.0.0-SNAPSHOT.jar:kurento-client-6.0.0-SNAPSHOT.jar:target/kms-monitoring-java-6.0.0-SNAPSHOT.jar org.kurento.tutorial.kmsmonitor.SendStats >> /var/log/sendstats.log 2>&1 &
